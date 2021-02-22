28079e9348ee77af97f3ce14e0cbc902fc7afa16
mlapalus <mlapalus@yahoo.fr>
2021-02-22 18:11:34 +0100

    update homepageresponse

--- domain/src/Response/HomePageResponse.php
+++ domain/src/Response/HomePageResponse.php
@@ -36,7 +36,8 @@ class HomePageResponse
             "userDescription" => "Jeune homme de 47ans, j'ai un diplôme d'ingénieur en électronique et en informatique,
                                   et un DEA en microélectronique. J'ai travaillé 5 ans dans les telecoms, 
                                   puis j'ai travaillé pendant 15 ans dans la gestion d'établissements de nuit à Paris. 
-                                   Depuis 1 an, je me consacre à un retour dans le milieu de l'informatique. ",
+                                   Depuis 1 an, je me consacre à un retour dans le milieu de l'informatique et du 
+                                   dévelopement. ",
             "tagData" => $this->tagData,
             "dataPortfolio" => $this->dataPortfolio
         ];
