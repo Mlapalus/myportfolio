f4fe6a3720fddb3a712ac2984d8f763f8b1c3e2d
mlapalus <mlapalus@yahoo.fr>
2021-02-23 09:55:49 +0100

    add phpdoc in TagsRepository.php file, and codacy.yml in github workflows

--- src/Infrastructure/Adapter/Repository/TagsRepository.php
+++ src/Infrastructure/Adapter/Repository/TagsRepository.php
@@ -11,28 +11,49 @@ use MLapalus\Portfolio\Domain\Gateway\TagGateway;
 use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidInterface;
 
+/**
+ * Class TagsRepository
+ * @package App\Infrastructure\Adapter\Repository
+ */
 class TagsRepository  extends ServiceEntityRepository implements TagGateway
 {
+    /**
+     * TagsRepository constructor.
+     * @param ManagerRegistry $registry
+     */
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, \App\Infrastructure\Doctrine\Entity\Portfolio::class);
     }
 
+    /**
+     * @param Tag $tag
+     */
     public function create(Tag $tag): void
     {
         // TODO: Implement create() method.
     }
 
+    /**
+     * @param Tag $tag
+     */
     public function update(Tag $tag): void
     {
         // TODO: Implement update() method.
     }
 
+    /**
+     * @param UuidInterface $id
+     * @return Tag|null
+     */
     public function getTagById(UuidInterface $id): ?Tag
     {
         // TODO: Implement getTagById() method.
     }
 
+    /**
+     * @return Tag[]
+     */
     public function getAll(): array
     {
         return [
